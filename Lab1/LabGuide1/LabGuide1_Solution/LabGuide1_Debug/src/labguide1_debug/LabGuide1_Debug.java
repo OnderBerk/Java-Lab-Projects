872b637e5fce27a0440679af71d5d6d7a8c2d0e5
Berk Onder <34274686+OnderBerk@users.noreply.github.com>
2019-03-04 23:25:45 +0300

    Add files via upload

---
+++ Lab1/LabGuide1/LabGuide1_Solution/LabGuide1_Debug/src/labguide1_debug/LabGuide1_Debug.java
@@ -0,0 +1,27 @@
+/*Debug */
+
+package labguide1_debug;
+
+public class LabGuide1_Debug {
+
+    public static void main(String[] args) {
+        
+        //initialization of variables
+        int num1=5;
+        int num2=2;
+        
+        //Sum of variables
+        int res=num1+num2;
+        
+        //Print the result after summation       
+        System.out.println("Result is : "+res);
+        
+        //Multiplication of variables
+        res= num1*num2;
+      
+        //Print the result after multiplication
+        System.out.println("After change, result is: "+res);
+        
+    }
+    
+}
